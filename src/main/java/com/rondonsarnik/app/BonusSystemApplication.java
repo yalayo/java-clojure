162ccc977155a87b0911e6cd6ea7bc16c74da8bc
yalayo <60822416+yalayo@users.noreply.github.com>
2023-06-05 07:08:48 +0000

    Adding files generated by Spring Initializer.

---
+++ src/main/java/com/rondonsarnik/app/BonusSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rondonsarnik.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonusSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonusSystemApplication.class, args);
+	}
+
+}
